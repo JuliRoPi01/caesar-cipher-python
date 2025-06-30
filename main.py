85276162157e529500bc0b67c077e0263052554e
JuliRoPi01 <juliropi01@gmail.com>
2025-06-30 16:31:58 -0500

    Added decoder

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ ABC = "abcdefghijklmnopqrstuvwxyz"
 
 def main() -> None:
     message: str = ask_message()
+    activity: str = ask_activity()
     shift: int = ask_shift()
-    code: str = encode_message(message, shift)
-    print(code)
+
+    if activity == "d":
+        shift *= -1
+
+    code: str = code_message(message, shift)
+    print(f"Code: {code}")
 
 def ask_message() -> str:
-    usr_message: str = input("Message to encode: ")
+    usr_message: str = input("Message to encode/decode: ")
     return usr_message
+
+def ask_activity() -> str:
+    while True:
+        usr_activity: str = input("Are you going to encode or decode (e or d): ").strip()
+        if usr_activity == "e" or usr_activity == "d":
+            return usr_activity
+        print("Write 'e' to encode or 'd' to decode")
 
 def ask_shift() -> int:
     while True:
@@ -22,7 +34,7 @@ def ask_shift() -> int:
         else:
             print("Shift must be a number")
 
-def encode_message(message, shift) -> str:
+def code_message(message, shift) -> str:
     code: str = ""
     for char in message:
         if char.lower() in ABC:
