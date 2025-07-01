de53261fcc6e81465dcf53e7cf5a1bbfc1e9761f
JuliRoPi01 <juliropi01@gmail.com>
2025-07-01 12:04:49 -0500

    added working tkinter gui
    working but not beautiful

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
 ABC = "abcdefghijklmnopqrstuvwxyz"
+FONT = ("comfortaa", 14)
+
 
 def main() -> None:
-    message: str = ask_message()
-    shift: int = ask_shift()
-    code: str = encode_message(message, shift)
-    print(code)
 
-def ask_message() -> str:
-    usr_message: str = input("Message to encode: ")
-    return usr_message
-
-def ask_shift() -> int:
-    while True:
-        usr_shift: str = input("Shift number: ")
-        if usr_shift.isdigit():
-            shift: int = int(usr_shift)
-            if shift >= 0:
-                return shift
-            else:
-                print("Number must be positive")
+    def get_shift() -> int | None:
+        usr_shift: str = shift_entry.get()
+        if not usr_shift:
+            messagebox.showerror("Error", "Enter a shift value")
+        elif usr_shift.isdigit():
+            return int(usr_shift)
         else:
-            print("Shift must be a number")
+            messagebox.showerror("Error", "Shift must be a positive integer")
 
-def encode_message(message, shift) -> str:
+    def code_message() -> None:
+        shift: int|None = get_shift()
+        if shift == None:
+            return
+        message: str = message_entry.get()
+        code = caesar_cipher(message= message, shift= shift)
+        result_label.config(text= code) 
+
+    root = tk.Tk()
+    root.title("Caesar Cipher")
+    root.config(padx= 20, pady= 20)
+
+    message_entry = tk.Entry(font= FONT)
+    message_entry.insert(0, "Message")
+    message_entry.grid(row= 0, column= 0)
+
+    shift_entry = tk.Entry(font=FONT)
+    shift_entry.grid(row= 0, column=1)
+
+    convert_btn = tk.Button(text= "Convert", command=code_message, font= FONT)
+    convert_btn.grid(row= 1, column= 0)
+
+    result_label = tk.Label(text="", font=FONT)
+    result_label.grid(row= 1, column= 1)
+
+    root.mainloop()
+
+def caesar_cipher(message, shift) -> str:
     code: str = ""
     for char in message:
         if char.lower() in ABC:
